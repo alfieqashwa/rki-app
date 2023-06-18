349b6969cde57c8a1255e4886b849ad3142097f9
alfieqashwa <alfieqashwa@gmail.com>
2023-06-18 14:35:23 +0700

    fix: modify layout of add-product dialog

--- src/components/quotation-list/add-quotation.tsx
+++ src/components/quotation-list/add-quotation.tsx
@@ -35,6 +35,7 @@ import { toast } from "~/ui/use-toast";
 import { api } from "~/utils/api";
 import { formattedInputPriceValue } from "~/utils/formattedInputValue";
 import { wait } from "~/utils/wait";
+import { ScrollArea } from "../ui/scroll-area";
 
 export const AddQuotation = (): JSX.Element => {
   const [open, setOpen] = useState(false);
@@ -141,7 +142,7 @@ export const AddQuotation = (): JSX.Element => {
           Add Quotation
         </Button>
       </SheetTrigger>
-      <SheetContent position="right" size="sm">
+      <SheetContent position="right" size="sm" className="w-2/3">
         <SheetHeader>
           <SheetTitle>Add New Quotation</SheetTitle>
           <SheetDescription>
@@ -150,239 +151,222 @@ export const AddQuotation = (): JSX.Element => {
         </SheetHeader>
         <form onSubmit={handleSubmit}>
           <div className="grid gap-4 py-4">
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label
-                htmlFor="orderNumber"
-                className="whitespace-nowrap text-right"
-              >
-                Order No
-              </Label>
-              <Input
-                id="orderNumber"
-                name="orderNumber"
-                placeholder="order number"
-                className="col-span-3 capitalize"
-              />
-              {error?.data?.zodError?.fieldErrors.name && (
-                <span className="col-span-4 -mt-2.5 text-right text-sm text-destructive">
-                  {error?.data?.zodError?.fieldErrors.name}
-                </span>
-              )}
+            <div className="grid grid-cols-2">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label
+                  htmlFor="orderNumber"
+                  className="whitespace-nowrap text-right"
+                >
+                  Order No
+                </Label>
+                <Input
+                  id="orderNumber"
+                  name="orderNumber"
+                  placeholder="order number"
+                  className="col-span-3 capitalize"
+                />
+                {error?.data?.zodError?.fieldErrors.name && (
+                  <span className="col-span-4 -mt-2.5 text-right text-sm text-destructive">
+                    {error?.data?.zodError?.fieldErrors.name}
+                  </span>
+                )}
+              </div>
+
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="dateOrdered" className="text-right">
+                  Date
+                </Label>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "w-[280px] justify-start text-left font-normal",
+                        !date && "text-muted-foreground"
+                      )}
+                    >
+                      <CalendarIcon className="mr-2 h-4 w-4" />
+                      {date ? format(date, "PPP") : <span>Pick a date</span>}
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0">
+                    <Calendar
+                      mode="single"
+                      selected={date}
+                      onSelect={setDate}
+                      initialFocus
+                    />
+                  </PopoverContent>
+                </Popover>
+              </div>
             </div>
 
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="dateOrdered" className="text-right">
-                Date
-              </Label>
-              <Popover>
-                <PopoverTrigger asChild>
-                  <Button
-                    variant={"outline"}
-                    className={cn(
-                      "w-[280px] justify-start text-left font-normal",
-                      !date && "text-muted-foreground"
-                    )}
-                  >
-                    <CalendarIcon className="mr-2 h-4 w-4" />
-                    {date ? format(date, "PPP") : <span>Pick a date</span>}
-                  </Button>
-                </PopoverTrigger>
-                <PopoverContent className="w-auto p-0">
-                  <Calendar
-                    mode="single"
-                    selected={date}
-                    onSelect={setDate}
-                    initialFocus
-                  />
-                </PopoverContent>
-              </Popover>
+            <div className="grid grid-cols-2">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="companyId" className="text-right">
+                  Customer
+                </Label>
+                <Select name="companyId">
+                  <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="Customer" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {companiesQuery.status === "success" &&
+                      companiesQuery.data.map((company) => (
+                        <SelectItem
+                          className="whitespace-nowrap pl-2 capitalize"
+                          value={company.id}
+                          key={company.id}
+                        >
+                          <div className="flex w-full items-center">
+                            <Building className="mr-2 h-4 w-4 text-muted-foreground" />
+                            <span className="whitespace-nowrap capitalize">
+                              {company.name}
+                            </span>
+                          </div>
+                        </SelectItem>
+                      ))}
+                  </SelectContent>
+                </Select>
+                {error?.data?.zodError?.fieldErrors.companyId && (
+                  <span className="col-span-4 -mt-4 text-right text-sm text-destructive">
+                    {error?.data?.zodError?.fieldErrors.companyId}
+                  </span>
+                )}
+              </div>
+
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="userId" className="text-right">
+                  User
+                </Label>
+                <Select name="userId">
+                  <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="User" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {usersQuery.status === "success" &&
+                      usersQuery.data.map((user) => (
+                        <SelectItem
+                          className="whitespace-nowrap pl-2 capitalize"
+                          value={user.id}
+                          key={user.id}
+                        >
+                          <div className="flex w-full items-center">
+                            <User className="mr-2 h-4 w-4 text-muted-foreground" />
+                            <span className="whitespace-nowrap capitalize">
+                              {user.name}
+                            </span>
+                          </div>
+                        </SelectItem>
+                      ))}
+                  </SelectContent>
+                </Select>
+                {error?.data?.zodError?.fieldErrors.userId && (
+                  <span className="col-span-4 -mt-4 text-right text-sm text-destructive">
+                    {error?.data?.zodError?.fieldErrors.userId}
+                  </span>
+                )}
+              </div>
             </div>
 
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="companyId" className="text-right">
-                Customer
-              </Label>
-              <Select name="companyId">
-                <SelectTrigger className="w-[180px]">
-                  <SelectValue placeholder="Customer" />
-                </SelectTrigger>
-                <SelectContent>
-                  {companiesQuery.status === "success" &&
-                    companiesQuery.data.map((company) => (
-                      <SelectItem
-                        className="whitespace-nowrap pl-2 capitalize"
-                        value={company.id}
-                        key={company.id}
-                      >
-                        <div className="flex w-full items-center">
-                          <Building className="mr-2 h-4 w-4 text-muted-foreground" />
-                          <span className="whitespace-nowrap capitalize">
-                            {company.name}
-                          </span>
-                        </div>
-                      </SelectItem>
-                    ))}
-                </SelectContent>
-              </Select>
-              {error?.data?.zodError?.fieldErrors.companyId && (
-                <span className="col-span-4 -mt-4 text-right text-sm text-destructive">
-                  {error?.data?.zodError?.fieldErrors.companyId}
-                </span>
-              )}
-            </div>
-
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="userId" className="text-right">
-                User
-              </Label>
-              <Select name="userId">
-                <SelectTrigger className="w-[180px]">
-                  <SelectValue placeholder="User" />
-                </SelectTrigger>
-                <SelectContent>
-                  {usersQuery.status === "success" &&
-                    usersQuery.data.map((user) => (
-                      <SelectItem
-                        className="whitespace-nowrap pl-2 capitalize"
-                        value={user.id}
-                        key={user.id}
-                      >
-                        <div className="flex w-full items-center">
-                          <User className="mr-2 h-4 w-4 text-muted-foreground" />
-                          <span className="whitespace-nowrap capitalize">
-                            {user.name}
-                          </span>
-                        </div>
-                      </SelectItem>
-                    ))}
-                </SelectContent>
-              </Select>
-              {error?.data?.zodError?.fieldErrors.userId && (
-                <span className="col-span-4 -mt-4 text-right text-sm text-destructive">
-                  {error?.data?.zodError?.fieldErrors.userId}
-                </span>
-              )}
-            </div>
             <SheetHeader className="mt-4">
               <SheetTitle>Order Item(s)</SheetTitle>
               <SheetDescription>
                 Fill the form to create order item(s) button.
               </SheetDescription>
             </SheetHeader>
-            {inputOrderItemsFields.map((input, i) => (
-              <Fragment key={i}>
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Label htmlFor="productId" className="text-right">
-                    Product
-                  </Label>
-                  <select
-                    name="productId"
-                    value={input.productId}
-                    onChange={(e) => handleChange(i, e)}
-                  >
-                    {productsQuery.status === "success" &&
-                      productsQuery.data.map((product) => (
-                        <option value={product.id} key={product.id}>
-                          {product.name}
-                        </option>
-                      ))}
-                  </select>
+            <ScrollArea className="h-72 pr-6">
+              {inputOrderItemsFields.map((input, i) => (
+                <Fragment key={i}>
+                  <div className="grid grid-cols-2">
+                    <div className="grid grid-cols-4 items-center gap-4">
+                      <Label htmlFor="productId" className="text-right">
+                        Product
+                      </Label>
+                      <select
+                        name="productId"
+                        value={input.productId}
+                        onChange={(e) => handleChange(i, e)}
+                      >
+                        {productsQuery.status === "success" &&
+                          productsQuery.data.map((product) => (
+                            <option value={product.id} key={product.id}>
+                              {product.name}
+                            </option>
+                          ))}
+                      </select>
 
-                  {error?.data?.zodError?.fieldErrors.productId && (
-                    <span className="col-span-4 -mt-4 text-right text-sm text-destructive">
-                      {error?.data?.zodError?.fieldErrors.productId}
-                    </span>
-                  )}
-                </div>
+                      {error?.data?.zodError?.fieldErrors.productId && (
+                        <span className="col-span-4 -mt-4 text-right text-sm text-destructive">
+                          {error?.data?.zodError?.fieldErrors.productId}
+                        </span>
+                      )}
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                      <Label htmlFor="quantity" className="text-right">
+                        Qty
+                      </Label>
+                      <Input
+                        id="quantity"
+                        name="quantity"
+                        value={input.quantity}
+                        onChange={(e) => handleChange(i, e)}
+                        placeholder="quantity"
+                        className="col-span-3 capitalize"
+                      />
+                      {error?.data?.zodError?.fieldErrors.quantity && (
+                        <span className="col-span-4 -mt-4 text-right text-sm text-destructive">
+                          {error?.data?.zodError?.fieldErrors.quantity}
+                        </span>
+                      )}
+                    </div>
+                  </div>
 
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Label htmlFor="description" className="text-right">
-                    Description
-                  </Label>
-                  <Input
-                    id="description"
-                    name="description"
-                    value={input.description}
-                    onChange={(e) => handleChange(i, e)}
-                    placeholder="description"
-                    className="col-span-3 capitalize"
-                  />
-                  {error?.data?.zodError?.fieldErrors.description && (
-                    <span className="col-span-4 -mt-4 text-right text-sm text-destructive">
-                      {error?.data?.zodError?.fieldErrors.description}
-                    </span>
-                  )}
-                </div>
-
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Label htmlFor="quantity" className="text-right">
-                    Qty
-                  </Label>
-                  <Input
-                    id="quantity"
-                    name="quantity"
-                    value={input.quantity}
-                    onChange={(e) => handleChange(i, e)}
-                    placeholder="quantity"
-                    className="col-span-3 capitalize"
-                  />
-                  {error?.data?.zodError?.fieldErrors.quantity && (
-                    <span className="col-span-4 -mt-4 text-right text-sm text-destructive">
-                      {error?.data?.zodError?.fieldErrors.quantity}
-                    </span>
-                  )}
-                </div>
-                <div
-                  className={`flex w-full items-center justify-end space-x-4`}
-                >
-                  <Button
-                    type="button"
-                    disabled={inputOrderItemsFields.length === 1}
-                    size="sm"
-                    onClick={() => handleRemoveFields(i)}
-                    className="w-16"
-                  >
-                    <MinusCircle />
-                  </Button>
-                  <Button
-                    type="button"
-                    disabled={
-                      input.productId === "" ||
-                      input.description === "" ||
-                      input.quantity === ""
-                    }
-                    size="sm"
-                    onClick={handleAddFields}
-                    className="w-16"
-                  >
-                    <PlusCircle />
-                  </Button>
-                </div>
-              </Fragment>
-            ))}
-
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label
-                htmlFor="totalPrice"
-                className="whitespace-nowrap text-right"
-              >
-                Total Price
-              </Label>
-              <Input
-                id="totalPrice"
-                name="totalPrice"
-                type="text"
-                value={inputTotalPrice}
-                onChange={handleTotalPriceChange}
-                placeholder="cost price"
-                className="col-span-3"
-              />
-              {error?.data?.zodError?.fieldErrors.totalPrice && (
-                <span className="col-span-4 -mt-2.5 text-right text-sm text-destructive">
-                  {error?.data?.zodError?.fieldErrors.totalPrice}
-                </span>
-              )}
-            </div>
+                  <div className="mt-4 grid grid-cols-7 items-center gap-4">
+                    <Label htmlFor="description" className="text-right">
+                      Description
+                    </Label>
+                    <Input
+                      id="description"
+                      name="description"
+                      value={input.description}
+                      onChange={(e) => handleChange(i, e)}
+                      placeholder="description"
+                      className="col-span-6 capitalize"
+                    />
+                    {error?.data?.zodError?.fieldErrors.description && (
+                      <span className="col-span-4 -mt-4 text-right text-sm text-destructive">
+                        {error?.data?.zodError?.fieldErrors.description}
+                      </span>
+                    )}
+                  </div>
+                  <div className="mb-8 mt-4 flex w-full items-center justify-end space-x-4">
+                    <Button
+                      type="button"
+                      disabled={inputOrderItemsFields.length === 1}
+                      size="sm"
+                      onClick={() => handleRemoveFields(i)}
+                      className="w-16"
+                    >
+                      <MinusCircle />
+                    </Button>
+                    <Button
+                      type="button"
+                      disabled={
+                        input.productId === "" ||
+                        input.description === "" ||
+                        input.quantity === ""
+                      }
+                      size="sm"
+                      onClick={handleAddFields}
+                      className="w-16"
+                    >
+                      <PlusCircle />
+                    </Button>
+                  </div>
+                </Fragment>
+              ))}
+            </ScrollArea>
           </div>
           <SheetFooter className="absolute bottom-16 right-8 w-full">
             <Button type="button" variant="ghost" onClick={handleCancel}>
