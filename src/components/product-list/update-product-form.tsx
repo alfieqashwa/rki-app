6dcb3a23fdd13bb0cafa82a43066b651330a3b67
alfieqashwa <alfieqashwa@gmail.com>
2023-06-28 16:28:55 +0700

    fix: types error on deployment

--- src/components/product-list/update-product-form.tsx
+++ src/components/product-list/update-product-form.tsx
@@ -157,7 +157,10 @@ export function UpdateProductForm({
           render={({ field }) => (
             <FormItem className="grid grid-cols-6 items-center gap-4">
               <FormLabel className="mt-2 text-right">Customer</FormLabel>
-              <Select onValueChange={field.onChange} defaultValue={field.value}>
+              <Select
+                onValueChange={(value) => field.onChange(value as UomType)}
+                defaultValue={field.value}
+              >
                 <FormControl className="col-span-3 w-[240px] capitalize">
                   <SelectTrigger>
                     <SelectValue placeholder="Select product" />
